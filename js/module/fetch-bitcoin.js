ec215a07dd2352eb98cb4978b36918dcb47bcc39
micael1ma <micael1ma.professional@gmail.com>
2025-10-09 20:42:31 -0300

    fetchBitcoin refatorado

--- js/module/fetch-bitcoin.js
+++ js/module/fetch-bitcoin.js
@@ -1,15 +1,9 @@
-export default function initFetchBitcoin() {
-  async function fetchBitcoin(url) {
-    try {
-      const bitcoinResponse = await fetch(url);
-      const bitcoinJSON = await bitcoinResponse.json();
-
-      const btcPreco = document.querySelector(".btc-preco");
-      btcPreco.innerText = (1000 / bitcoinJSON.BRL.buy).toFixed(4);
-    } catch (erro) {
-      console.log(erro);
-    }
-  }
-
-  fetchBitcoin("https://blockchain.info/ticker");
+export default function fetchBitcoin(url, target) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((bitcoin) => {
+      const btcPreco = document.querySelector(target);
+      btcPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
+    })
+    .catch((error) => console.log(Error(error)));
 }
--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ import initDropdownMenu from "./module/dropdown-menu.js";
 import initMenuMobile from "./module/menu-mobile.js";
 import initFuncionamento from "./module/funcionamento.js";
 import fetchAnimais from "./module/fetch-animais.js";
-import initFetchBitcoin from "./module/fetch-bitcoin.js";
+import fetchBitcoin from "./module/fetch-bitcoin.js";
 import initAnimacaoInitScroll from "./module/scroll-animacao.js";
 
 const scrollsuave = new ScrollSuave("[data-menu='suave'] a[href^='#']");
@@ -35,7 +35,8 @@ toolTip.init();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchBitcoin();
 initAnimacaoInitScroll();
 
 fetchAnimais("../../animaisapi.json", ".numeros-grid");
+
+fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
