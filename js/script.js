9d2198f8d4c5c4787043dc34ba4fd8ecfa82f35b
micael1ma <micael1ma.professional@gmail.com>
2025-10-11 13:05:23 -0300

    Funcionamento transformado em classe

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import Modal from "./module/modal.js";
 import ToolTip from "./module/tooltip.js";
 import DropdownMenu from "./module/dropdown-menu.js";
 import MenuMobile from "./module/menu-mobile.js";
-import initFuncionamento from "./module/funcionamento.js";
+import Funcionamento from "./module/funcionamento.js";
 import fetchAnimais from "./module/fetch-animais.js";
 import fetchBitcoin from "./module/fetch-bitcoin.js";
 import ScrollAnima from "./module/scroll-anima.js";
@@ -41,7 +41,9 @@ dropdownMenu.init();
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
 
-initFuncionamento();
+const funcionamento = new Funcionamento("[data-semana]", "aberto");
+
+funcionamento.init();
 
 fetchAnimais("../../animaisapi.json", ".numeros-grid");
 
