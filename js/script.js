47fb351022900546ca3138c0bcbd7fe4735c1479
micael1ma <micael1ma.professional@gmail.com>
2025-11-19 21:54:54 -0300

    adicionado slide de fotos

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
-import ScrollSuave from "./module/scroll-suave.js";
-import Accordion from "./module/accordion.js";
-import TabNav from "./module/tabnav.js";
-import Modal from "./module/modal.js";
-import ToolTip from "./module/tooltip.js";
-import DropdownMenu from "./module/dropdown-menu.js";
-import MenuMobile from "./module/menu-mobile.js";
-import Funcionamento from "./module/funcionamento.js";
-import fetchAnimais from "./module/fetch-animais.js";
-import fetchBitcoin from "./module/fetch-bitcoin.js";
-import ScrollAnima from "./module/scroll-anima.js";
+import ScrollSuave from './module/scroll-suave.js';
+import Accordion from './module/accordion.js';
+import TabNav from './module/tabnav.js';
+import Modal from './module/modal.js';
+import ToolTip from './module/tooltip.js';
+import DropdownMenu from './module/dropdown-menu.js';
+import MenuMobile from './module/menu-mobile.js';
+import Funcionamento from './module/funcionamento.js';
+import fetchAnimais from './module/fetch-animais.js';
+import fetchBitcoin from './module/fetch-bitcoin.js';
+import ScrollAnima from './module/scroll-anima.js';
+
+import SlideNav from './module/slide.js';
 
 const scrollsuave = new ScrollSuave("[data-menu='suave'] a[href^='#']");
 scrollsuave.init();
@@ -18,33 +20,37 @@ accordion.init();
 
 const tabNav = new TabNav(
   "[data-tab='menu'] li",
-  "[data-tab='content'] section"
+  "[data-tab='content'] section",
 );
 tabNav.init();
 
 const modal = new Modal(
   "[data-modal='abrir']",
   "[data-modal='fechar']",
-  "[data-modal='container']"
+  "[data-modal='container']",
 );
 modal.init();
 
-const toolTip = new ToolTip("[data-tooltip");
+const toolTip = new ToolTip('[data-tooltip');
 toolTip.init();
 
 const scrollAnima = new ScrollAnima("[data-anime='scroll']");
 scrollAnima.init();
 
-const dropdownMenu = new DropdownMenu("[data-dropdown]");
+const dropdownMenu = new DropdownMenu('[data-dropdown]');
 dropdownMenu.init();
 
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
 
-const funcionamento = new Funcionamento("[data-semana]", "aberto");
-
+const funcionamento = new Funcionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
-fetchAnimais("../../animaisapi.json", ".numeros-grid");
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 
-fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+
+const slide = new SlideNav('.slide', '.slide-wrapper');
+slide.init();
+
+slide.addControl('.custom-controls');
